93589f845664556972a11ae7e56867bc0354a718
Bruno Lopes <bruno_lopes87@outlook.com>
2024-10-30 09:12:32 -0300

    add swagger

---
+++ src/main/java/io/github/brunolopes/imageliteapi/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package io.github.brunolopes.imageliteapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("API Documentation")
+                        .version("1.0.0")
+                        .description("Documentação da API do projeto ImageLiteAPI")
+                );
+    }
+}
